1a6a3501274e79c732730ab872aecc2aaef4869c
IbnuJabir <kedirjabir12@gmail.com>
2025-04-21 02:57:46 +0800

    feat: update Redis connection pool initialization to use from_url with parameters

--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -37,9 +37,12 @@ async def redis_lifespan(app: FastAPI):
     global redis_pool
     print("Initializing Redis connection pool...")
     try:
+            # f"redis://:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:{settings.REDIS_PORT}/0",
         redis_pool = ConnectionPool.from_url(
-            f"redis://:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:{settings.REDIS_PORT}/0",
-            decode_responses=False,
+            host=settings.REDIS_HOST, 
+            port=settings.REDIS_PORT, 
+            password=settings.REDIS_PASSWORD,
+            decode_responses=True,
             max_connections=20
         )
         # Test the connection
